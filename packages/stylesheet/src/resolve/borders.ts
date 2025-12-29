0b30525338c5033b0fe9233c8bd8a1f525b40596
Nilesh Rathod <Nilesh.Rathod@tts-talent.com>
2025-12-29 06:39:31 +0000

    move Radius let to const

--- packages/stylesheet/src/resolve/borders.ts
+++ packages/stylesheet/src/resolve/borders.ts
@@ -74,7 +74,7 @@ const resolveBorderShorthand = <K extends BorderKey>(
     }
 
     if (key.match(/Radius$/)) {
-      let radius = value ? transformUnit(container, value) : undefined;
+      const radius = value ? transformUnit(container, value) : undefined;
 
       if (typeof radius !== 'number')
         throw new Error(`Invalid border radius: ${radius}`);
